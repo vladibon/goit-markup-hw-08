8224635df950f3b20e714a26671edf6395427fd4
Vladimir Bondar <vl_bondar@hotmail.com>
2021-05-07 18:14:00 +0300

    Add mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,7 +5,7 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("menu-btn--is-open");
+    menuBtnRef.classList.toggle("menu__btn--is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("menu--is-open");
