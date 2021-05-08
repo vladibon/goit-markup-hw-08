52cab826d2d8e6383a4a940464a785eb032aa8d3
Vladimir Bondar <vl_bondar@hotmail.com>
2021-05-08 22:24:29 +0300

    Add adaptive header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,9 +5,9 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("menu__btn--is-open");
+    menuBtnRef.classList.toggle("mobile-menu__btn--is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle("menu--is-open");
+    mobileMenuRef.classList.toggle("mobile-menu--is-open");
   });
 })();
